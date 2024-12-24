275ce3c3f5167c938df57abe5715ecf7c919b72c
zwindl <zwindl@hotmail.com>
2024-12-24 22:25:37 +0800

    refactor! Change git lib from sync to async

--- lua/orphan/git.lua
+++ lua/orphan/git.lua
@@ -6,12 +6,20 @@ git.is_git_dir = function(path)
 end
 
 -- query the last commit time of a path
-git.last_commit_time = function(path)
+git.last_commit_time_async = function(path, callback)
     -- it returns a timestamp like `1734691156`
-    -- translate timestamp back to date string with:
-    -- vim.print(os.date("%Y-%m-%d", 1734691156))
     -- TODO: consider return more info like commit message based on options
-    return vim.fn.system("git log -1 --format=%ct " .. path)
+    vim.system(
+        { "git", "log", "-1", "--format=%ct ", path },
+        { cwd = path },
+        function(rst)
+            --TODO: handle error
+            if rst.code ~= 0 then
+                callback(nil)
+            end
+            callback(rst.stdout)
+        end
+    )
 end
 
 return git
